89e202d78c63259f6d5d481bc57e0ab377912991
tkmik <tkach_nikita@icloud.com>
2020-03-25 14:36:26 +0300

    method Menu() transfer to abstract class

--- CSharpTree/ServiceLibrary/BinaryTree/BaseBinaryTree.cs
+++ CSharpTree/ServiceLibrary/BinaryTree/BaseBinaryTree.cs
@@ -5,12 +5,14 @@ namespace ServiceLibrary.BinaryTree
 {
     public abstract class BaseBinaryTree<T> where T :IComparable
     {
-        public BinaryTree<T> BinaryTree = new BinaryTree<T>();
-        public void Search(T count) => BinaryTree.Search(count);
-        public void Remove(T count) => BinaryTree.Remove(count);
-        public void PrintValues() => BinaryTree.PrintValues();
+        protected BinaryTree<T> BinaryTree = new BinaryTree<T>();
+        protected void Search(T count) => BinaryTree.Search(count);
+        protected void Remove(T count) => BinaryTree.Remove(count);
+        protected void PrintValues() => BinaryTree.PrintValues();
         protected abstract BinaryTree<T> MakingTree();
         protected abstract BinaryTree<T> GeneratedTree();
         protected abstract BinaryTree<T> CreatedTree();
+
+        public abstract void Menu();
     }
 }
