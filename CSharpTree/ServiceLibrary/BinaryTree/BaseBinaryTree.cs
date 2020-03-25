6eeb9effcf7db2e80069aba6e1631a802a81c4cc
tkmik <tkach_nikita@icloud.com>
2020-03-25 13:58:00 +0300

    added abstract class for binary tree

---
+++ CSharpTree/ServiceLibrary/BinaryTree/BaseBinaryTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreeLibrary;
+
+namespace ServiceLibrary.BinaryTree
+{
+    public abstract class BaseBinaryTree<T> where T :IComparable
+    {
+        public BinaryTree<T> BinaryTree = new BinaryTree<T>();
+        public void Search(T count) => BinaryTree.Search(count);
+        public void Remove(T count) => BinaryTree.Remove(count);
+        public void PrintValues() => BinaryTree.PrintValues();
+        protected abstract BinaryTree<T> MakingTree();
+        protected abstract BinaryTree<T> GeneratedTree();
+        protected abstract BinaryTree<T> CreatedTree();
+    }
+}
