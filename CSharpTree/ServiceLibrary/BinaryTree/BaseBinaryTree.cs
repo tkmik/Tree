44a1af10662a2fad0a3ec3e0a852ae5e0be3035e
tkmik <tkach_nikita@icloud.com>
2020-03-26 14:03:11 +0300

    removed variable, added auto-property

--- CSharpTree/ServiceLibrary/BinaryTree/BaseBinaryTree.cs
+++ CSharpTree/ServiceLibrary/BinaryTree/BaseBinaryTree.cs
@@ -3,9 +3,13 @@ using TreeLibrary;
 
 namespace ServiceLibrary.BinaryTree
 {
+    /// <summary>
+    /// базовые свойства и методы для бинарного дерева
+    /// </summary>
+    /// <typeparam name="T">тип</typeparam>
     public abstract class BaseBinaryTree<T> where T :IComparable
-    {
-        protected BinaryTree<T> BinaryTree = new BinaryTree<T>();
+    {        
+        protected BinaryTree<T> BinaryTree { get; set; }
         protected void Search(T count) => BinaryTree.Search(count);
         protected void Remove(T count) => BinaryTree.Remove(count);
         protected void PrintValues() => BinaryTree.PrintValues();
